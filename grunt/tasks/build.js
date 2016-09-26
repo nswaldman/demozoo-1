63d388e841cb38606a5eeaa63b29bdeb061adafa
nuey <nswaldman@gmail.com>
2016-09-26 21:06:30 +0200

    Added build task.
    todo: automate a first build run for new developer instance.

--- grunt/tasks/build.js
+++ grunt/tasks/build.js
@@ -1,9 +1,8 @@
 module.exports = function (grunt) {
 
-    grunt.registerTask('default', [
+    grunt.registerTask('build', [
         'less',
         'svgmin',
-        'svgstore',
-        'watch'
+        'svgstore'
     ]);
 };
